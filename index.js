f775ebe7f427a93fc07f390b782d77298834815b
Timilehin Olayuwa <timileyinolayuwa@gmail.com>
2024-03-07 18:23:17 +0100

    server code modified

--- index.js
+++ index.js
@@ -194,33 +194,39 @@ const fetchTokenMiddleware = async (req, res, next) => {
 
 // Use the middleware for protected routes
 app.use("/api/dashboard", verifyToken, fetchTokenMiddleware);
-app.post('/api/submit-quiz/:chapter', verifyToken, async (req, res) => {
+app.post("/api/submit-quiz/:chapter", verifyToken, async (req, res) => {
   const { userId } = req.body;
-    const { chapter } = req.params;
-    const { score } = req.body;
+  const { chapter } = req.params;
+  const { score } = req.body;
 
   try {
-      // Check if the user has submitted the quiz twice in the last hour
-      const lastTwoSubmissions = await Score.find({
-          userId,
-          createdAt: { $gte: new Date(Date.now() - 3600000) }, // One hour ago
-      }).sort({ createdAt: 'desc' }).limit(2);
+    // Check if the user has submitted the quiz twice in the last hour
+    const lastTwoSubmissions = await Score.find({
+      userId,
+      createdAt: { $gte: new Date(Date.now() - 3600000) }, // One hour ago
+    })
+      .sort({ createdAt: "desc" })
+      .limit(2);
 
-      if (lastTwoSubmissions.length === 2) {
-          return res.status(400).json({ error: 'You have reached the limit of quiz attempts in one hour' });
-      }
+    if (lastTwoSubmissions.length === 2) {
+      return res
+        .status(400)
+        .json({
+          error: "You have reached the limit of quiz attempts in one hour",
+        });
+    }
 
-      // Update the score in the database
-      const updatedScore = await Score.findOneAndUpdate(
-          { userId },
-          { $inc: { [chapter]: score } },
-          { new: true, upsert: true }
-      );
+    // Update the score in the database
+    const updatedScore = await Score.findOneAndUpdate(
+      { userId },
+      { $inc: { [chapter]: score } },
+      { new: true, upsert: true }
+    );
 
-      res.json({ message: 'Quiz submitted successfully', score: updatedScore });
+    res.json({ message: "Quiz submitted successfully", score: updatedScore });
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
@@ -251,7 +257,6 @@ app.get("/api/dashboard", async (req, res) => {
 
 //   res.status(200).json({ success: true });
 // });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
