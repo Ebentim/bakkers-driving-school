5351f7a0224fb700ab987eb72711bde7cb213b53
Timilehin Olayuwa <timileyinolayuwa@gmail.com>
2024-03-27 01:21:36 +0100

    administrative dashboard

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -47,6 +48,8 @@ mongoose
 const Profile = require("./models/profile.model");
 const Token = require("./models/token.model");
 const Score = require("./models/score.model");
+const Dashboard = require("./models/admin.model");
+const AdminToken = require("./models/adminToken.model");
 
 const saveOrUpdateTokenToDatabase = async (userId, token) => {
   try {
@@ -87,6 +90,147 @@ const fetchTokenFromDatabase = async (userId) => {
   }
 };
 
+const saveOrUpdateAdminTokenToDatabase = async (userId, token) => {
+  try {
+    const existingToken = await AdminToken.findOne({ userId });
+
+    if (existingToken) {
+      existingToken.accessToken = token;
+      existingToken.expiresAt = Date.now() + 3600000;
+      await existingToken.save();
+    } else {
+      const newToken = new Token({
+        userId,
+        accessToken: token,
+        expiresAt: Date.now() + 3600000,
+      });
+
+      await newToken.save();
+    }
+  } catch (error) {
+    throw new Error(
+      "Error saving or updating token to the database: " + error.message
+    );
+  }
+};
+
+const fetchAdminTokenFromDatabase = async (userId) => {
+  try {
+    const tokenDoc = await AdminToken.findOne({ userId });
+
+    if (tokenDoc) {
+      return tokenDoc.accessToken;
+    } else {
+      throw new Error("Token not found for user");
+    }
+  } catch (error) {
+    throw new Error("Error fetching token from the database: " + error.message);
+  }
+};
+app.post("/api/adminsignup", cors(corsOptions), async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+  if (!firstname || !lastname || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new Dashboard({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(201).json({ message: "User created successfully!" });
+    console.log(user);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    if (error.code === 11000 && error.keyPattern && error.keyPattern.email) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+app.get("/api/adminsignup", (req, res) => {
+  res.setHeader("Cache-Control", "no-store");
+  res.json();
+});
+
+app.get("/api/get-tokens", cors(corsOptions), async (req, res) => {
+  try {
+    // Find all tokens
+    const tokens = await Token.find();
+
+    if (tokens.length === 0) {
+      return res.status(404).json({ message: "No tokens found" });
+    }
+
+    res.status(200).json({ tokens });
+  } catch (error) {
+    console.error("Error fetching tokens:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+app.post("/api/adminsignin", cors(corsOptions), async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await Dashboard.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userid: user._id, email: user.email }, secretKey, {
+      expiresIn: "1h",
+    });
+    await saveOrUpdateAdminTokenToDatabase(user._id, token);
+    res.status(200).json({ message: "Sign-in successful", accessToken: token });
+  } catch (error) {
+    console.error("Error during sign-in:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+const fetchAdminTokenMiddleware = async (req, res, next) => {
+  const userId = req.user.userid;
+
+  try {
+    const token = await fetchAdminTokenFromDatabase(userId);
+
+    if (token.expiresAt < Date.now()) {
+      await Token.deleteOne({ userId });
+      throw new Error("Token has expired");
+    }
+    req.accessToken = token.accessToken;
+    next();
+  } catch (error) {
+    console.error("Error fetching token:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// app.get("/api/admindashboard", cors(corsOptions), async (req, res) => {
+//   const accessToken = req.accessToken;
+//   try {
+//     const user = await Dashboard.findOne({ email: req.user.email });
+
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     res.status(200).json({ user });
+//   } catch (error) {
+//     console.error("Error fetching user data:", error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// });
+
+// students
 app.post("/api/signup", cors(corsOptions), async (req, res) => {
   const { firstname, lastname, address, email, password, birthdate } = req.body;
 
@@ -195,8 +339,30 @@ const fetchTokenMiddleware = async (req, res, next) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+// app.use("/api/admindashboard", verifyToken, fetchAdminTokenMiddleware);
 
 app.use("/api/dashboard", verifyToken, fetchTokenMiddleware);
+
+app.get(
+  "/api/admindashboard",
+  cors(corsOptions),
+  verifyToken,
+  async (req, res) => {
+    try {
+      // Find the user by the userId stored in req.user
+      const user = await Dashboard.findOne({ _id: req.user.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.status(200).json({ user });
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
 
 app.get("/api/dashboard", cors(corsOptions), async (req, res) => {
   const accessToken = req.accessToken;
@@ -210,6 +376,43 @@ app.get("/api/dashboard", cors(corsOptions), async (req, res) => {
     res.status(200).json({ user });
   } catch (error) {
     console.error("Error fetching user data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get(
+  "/api/profilesforadmindashboard",
+  cors(corsOptions),
+
+  async (req, res) => {
+    try {
+      // Find all profiles
+      const profiles = await Profile.find();
+
+      if (profiles.length === 0) {
+        return res.status(404).json({ message: "No profiles found" });
+      }
+
+      res.status(200).json({ profiles });
+    } catch (error) {
+      console.error("Error fetching profiles:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+app.get("/api/get-user-scores-admin", cors(corsOptions), async (req, res) => {
+  try {
+    // Find all scores
+    const scores = await Score.find();
+
+    if (scores.length === 0) {
+      return res.status(404).json({ message: "No scores found" });
+    }
+
+    res.status(200).json({ scores });
+  } catch (error) {
+    console.error("Error fetching scores:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
